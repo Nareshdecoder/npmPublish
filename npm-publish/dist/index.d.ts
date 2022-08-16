25d1908234a549eba8a2f6563b6dbfe92d14a5df
Naresh <naresh.r@10decoders.in>
2022-08-16 16:05:41 +0530

    folder structure

--- npm-publish/dist/index.d.ts
+++
@@ -1,2 +0,0 @@
-import Header from "./Hearder";
-export { Header };
--- npm-publish/dist/sessionStorage.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const saveToSession: (label: any, data: any) => void;
-export declare const removeToSession: (label: any) => void;
-export declare const getFromSession: (label: any) => string | null;
